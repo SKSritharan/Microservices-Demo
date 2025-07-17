007536e1cdb55a156d97c0f126fa9b1036086dd4
Sritharan Thiyagarajah <sritharan.thiyagarajah@maytech.au>
2025-07-17 15:11:19 +0530

    Added Circuit Breaker with Resilience4J

--- order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
@@ -4,9 +4,12 @@ import com.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +21,18 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        var factory = new org.springframework.http.client.SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+        factory.setReadTimeout(Duration.ofSeconds(3));
+        return factory;
     }
 
 }
